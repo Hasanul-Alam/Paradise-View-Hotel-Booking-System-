bdcb15e7513851002200f3c84e71399c1d892889
Hasanul-Alam <superdev0000@gmail.com>
2024-11-05 14:20:25 +0600

    Main body cards are done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import Banner from "./components/Banner/Banner";
+import Body from "./components/Body/Body";
 import Navbar from "./components/shared/Navbar/Navbar";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <>
       <Navbar />
       <Banner />
+      <Body />
     </>
   );
 }
---
+++ src/components/Body/Body.jsx
@@ -0,0 +1,343 @@
+import { PiTelevisionSimple } from "react-icons/pi";
+import { PiShower } from "react-icons/pi";
+import { IoWifiOutline } from "react-icons/io5";
+
+export default function Body() {
+  return (
+    <div className="w-[80%] mx-auto py-10 max-md:w-[95%]">
+      <div className="grid grid-cols-4 gap-4 max-md:grid-cols-1">
+        {/* Card */}
+        <div className="font-mulish">
+          {/* Card Image */}
+          <div>
+            <img
+              className="w-full"
+              src="https://i.ibb.co.com/DDsK8gr/Rectangle-26-1.png"
+              alt=""
+            />
+          </div>
+          <div className="px-3 py-3 shadow shadow-1">
+            {/* Title and avaibality */}
+            <div className="flex items-center justify-between">
+              <h2 className="text-xl text-[#7C6A46]">The Royal Room</h2>
+              <p className="text-sm text-[#7C6A46]">
+                Available: <span className="text-xs">Yes</span>
+              </p>
+            </div>
+            {/* Price */}
+            <p className="my-2 text-[#7C6A46] font-semibold text-lg">$120.32</p>
+            {/* Horizontal line */}
+            <hr className="w-full border border-1 border-[#7C6A46] mb-3" />
+            {/* Icons & Button */}
+            <div className="flex items-center justify-between">
+              {/* Icons */}
+              <div className="flex items-center">
+                <div className="bg-[#7C6A46] p-2 rounded-full text-white">
+                  <PiTelevisionSimple />
+                </div>
+                <div className="bg-[#7C6A46] p-2 rounded-full text-white mx-3">
+                  <PiShower />
+                </div>
+                <div className="bg-[#7C6A46] p-2 rounded-full text-white">
+                  <IoWifiOutline />
+                </div>
+              </div>
+              <button className="bg-[#7C6A46] text-white px-3 py-2 rounded hover:bg-[#8C6B27]">
+                Book Now
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="font-mulish">
+          {/* Card Image */}
+          <div>
+            <img
+              className="w-full"
+              src="https://i.ibb.co.com/DDsK8gr/Rectangle-26-1.png"
+              alt=""
+            />
+          </div>
+          <div className="px-3 py-3 shadow shadow-1">
+            {/* Title and avaibality */}
+            <div className="flex items-center justify-between">
+              <h2 className="text-xl text-[#7C6A46]">The Royal Room</h2>
+              <p className="text-sm text-[#7C6A46]">
+                Available: <span className="text-xs">Yes</span>
+              </p>
+            </div>
+            {/* Price */}
+            <p className="my-2 text-[#7C6A46] font-semibold text-lg">$120.32</p>
+            {/* Horizontal line */}
+            <hr className="w-full border border-1 border-[#7C6A46] mb-3" />
+            {/* Icons & Button */}
+            <div className="flex items-center justify-between">
+              {/* Icons */}
+              <div className="flex items-center">
+                <div className="bg-[#7C6A46] p-2 rounded-full text-white">
+                  <PiTelevisionSimple />
+                </div>
+                <div className="bg-[#7C6A46] p-2 rounded-full text-white mx-3">
+                  <PiShower />
+                </div>
+                <div className="bg-[#7C6A46] p-2 rounded-full text-white">
+                  <IoWifiOutline />
+                </div>
+              </div>
+              <button className="bg-[#7C6A46] text-white px-3 py-2 rounded hover:bg-[#8C6B27]">
+                Book Now
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="font-mulish">
+          {/* Card Image */}
+          <div>
+            <img
+              className="w-full"
+              src="https://i.ibb.co.com/DDsK8gr/Rectangle-26-1.png"
+              alt=""
+            />
+          </div>
+          <div className="px-3 py-3 shadow shadow-1">
+            {/* Title and avaibality */}
+            <div className="flex items-center justify-between">
+              <h2 className="text-xl text-[#7C6A46]">The Royal Room</h2>
+              <p className="text-sm text-[#7C6A46]">
+                Available: <span className="text-xs">Yes</span>
+              </p>
+            </div>
+            {/* Price */}
+            <p className="my-2 text-[#7C6A46] font-semibold text-lg">$120.32</p>
+            {/* Horizontal line */}
+            <hr className="w-full border border-1 border-[#7C6A46] mb-3" />
+            {/* Icons & Button */}
+            <div className="flex items-center justify-between">
+              {/* Icons */}
+              <div className="flex items-center">
+                <div className="bg-[#7C6A46] p-2 rounded-full text-white">
+                  <PiTelevisionSimple />
+                </div>
+                <div className="bg-[#7C6A46] p-2 rounded-full text-white mx-3">
+                  <PiShower />
+                </div>
+                <div className="bg-[#7C6A46] p-2 rounded-full text-white">
+                  <IoWifiOutline />
+                </div>
+              </div>
+              <button className="bg-[#7C6A46] text-white px-3 py-2 rounded hover:bg-[#8C6B27]">
+                Book Now
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="font-mulish">
+          {/* Card Image */}
+          <div>
+            <img
+              className="w-full"
+              src="https://i.ibb.co.com/DDsK8gr/Rectangle-26-1.png"
+              alt=""
+            />
+          </div>
+          <div className="px-3 py-3 shadow shadow-1">
+            {/* Title and avaibality */}
+            <div className="flex items-center justify-between">
+              <h2 className="text-xl text-[#7C6A46]">The Royal Room</h2>
+              <p className="text-sm text-[#7C6A46]">
+                Available: <span className="text-xs">Yes</span>
+              </p>
+            </div>
+            {/* Price */}
+            <p className="my-2 text-[#7C6A46] font-semibold text-lg">$120.32</p>
+            {/* Horizontal line */}
+            <hr className="w-full border border-1 border-[#7C6A46] mb-3" />
+            {/* Icons & Button */}
+            <div className="flex items-center justify-between">
+              {/* Icons */}
+              <div className="flex items-center">
+                <div className="bg-[#7C6A46] p-2 rounded-full text-white">
+                  <PiTelevisionSimple />
+                </div>
+                <div className="bg-[#7C6A46] p-2 rounded-full text-white mx-3">
+                  <PiShower />
+                </div>
+                <div className="bg-[#7C6A46] p-2 rounded-full text-white">
+                  <IoWifiOutline />
+                </div>
+              </div>
+              <button className="bg-[#7C6A46] text-white px-3 py-2 rounded hover:bg-[#8C6B27]">
+                Book Now
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="font-mulish">
+          {/* Card Image */}
+          <div>
+            <img
+              className="w-full"
+              src="https://i.ibb.co.com/DDsK8gr/Rectangle-26-1.png"
+              alt=""
+            />
+          </div>
+          <div className="px-3 py-3 shadow shadow-1">
+            {/* Title and avaibality */}
+            <div className="flex items-center justify-between">
+              <h2 className="text-xl text-[#7C6A46]">The Royal Room</h2>
+              <p className="text-sm text-[#7C6A46]">
+                Available: <span className="text-xs">Yes</span>
+              </p>
+            </div>
+            {/* Price */}
+            <p className="my-2 text-[#7C6A46] font-semibold text-lg">$120.32</p>
+            {/* Horizontal line */}
+            <hr className="w-full border border-1 border-[#7C6A46] mb-3" />
+            {/* Icons & Button */}
+            <div className="flex items-center justify-between">
+              {/* Icons */}
+              <div className="flex items-center">
+                <div className="bg-[#7C6A46] p-2 rounded-full text-white">
+                  <PiTelevisionSimple />
+                </div>
+                <div className="bg-[#7C6A46] p-2 rounded-full text-white mx-3">
+                  <PiShower />
+                </div>
+                <div className="bg-[#7C6A46] p-2 rounded-full text-white">
+                  <IoWifiOutline />
+                </div>
+              </div>
+              <button className="bg-[#7C6A46] text-white px-3 py-2 rounded hover:bg-[#8C6B27]">
+                Book Now
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="font-mulish">
+          {/* Card Image */}
+          <div>
+            <img
+              className="w-full"
+              src="https://i.ibb.co.com/DDsK8gr/Rectangle-26-1.png"
+              alt=""
+            />
+          </div>
+          <div className="px-3 py-3 shadow shadow-1">
+            {/* Title and avaibality */}
+            <div className="flex items-center justify-between">
+              <h2 className="text-xl text-[#7C6A46]">The Royal Room</h2>
+              <p className="text-sm text-[#7C6A46]">
+                Available: <span className="text-xs">Yes</span>
+              </p>
+            </div>
+            {/* Price */}
+            <p className="my-2 text-[#7C6A46] font-semibold text-lg">$120.32</p>
+            {/* Horizontal line */}
+            <hr className="w-full border border-1 border-[#7C6A46] mb-3" />
+            {/* Icons & Button */}
+            <div className="flex items-center justify-between">
+              {/* Icons */}
+              <div className="flex items-center">
+                <div className="bg-[#7C6A46] p-2 rounded-full text-white">
+                  <PiTelevisionSimple />
+                </div>
+                <div className="bg-[#7C6A46] p-2 rounded-full text-white mx-3">
+                  <PiShower />
+                </div>
+                <div className="bg-[#7C6A46] p-2 rounded-full text-white">
+                  <IoWifiOutline />
+                </div>
+              </div>
+              <button className="bg-[#7C6A46] text-white px-3 py-2 rounded hover:bg-[#8C6B27]">
+                Book Now
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="font-mulish">
+          {/* Card Image */}
+          <div>
+            <img
+              className="w-full"
+              src="https://i.ibb.co.com/DDsK8gr/Rectangle-26-1.png"
+              alt=""
+            />
+          </div>
+          <div className="px-3 py-3 shadow shadow-1">
+            {/* Title and avaibality */}
+            <div className="flex items-center justify-between">
+              <h2 className="text-xl text-[#7C6A46]">The Royal Room</h2>
+              <p className="text-sm text-[#7C6A46]">
+                Available: <span className="text-xs">Yes</span>
+              </p>
+            </div>
+            {/* Price */}
+            <p className="my-2 text-[#7C6A46] font-semibold text-lg">$120.32</p>
+            {/* Horizontal line */}
+            <hr className="w-full border border-1 border-[#7C6A46] mb-3" />
+            {/* Icons & Button */}
+            <div className="flex items-center justify-between">
+              {/* Icons */}
+              <div className="flex items-center">
+                <div className="bg-[#7C6A46] p-2 rounded-full text-white">
+                  <PiTelevisionSimple />
+                </div>
+                <div className="bg-[#7C6A46] p-2 rounded-full text-white mx-3">
+                  <PiShower />
+                </div>
+                <div className="bg-[#7C6A46] p-2 rounded-full text-white">
+                  <IoWifiOutline />
+                </div>
+              </div>
+              <button className="bg-[#7C6A46] text-white px-3 py-2 rounded hover:bg-[#8C6B27]">
+                Book Now
+              </button>
+            </div>
+          </div>
+        </div>
+
+        {/* Main card here. */}
+        <div className="font-mulish">
+          {/* Card Image */}
+          <div>
+            <img
+              className="w-full"
+              src="https://i.ibb.co.com/DDsK8gr/Rectangle-26-1.png"
+              alt=""
+            />
+          </div>
+          <div className="px-3 py-3 shadow shadow-1">
+            {/* Title and avaibality */}
+            <div className="flex items-center justify-between">
+              <h2 className="text-xl text-[#7C6A46]">The Royal Room</h2>
+              <p className="text-sm text-[#7C6A46]">
+                Available: <span className="text-xs">Yes</span>
+              </p>
+            </div>
+            {/* Price */}
+            <p className="my-2 text-[#7C6A46] font-semibold text-lg">$120.32</p>
+            {/* Horizontal line */}
+            <hr className="w-full border border-1 border-[#7C6A46] mb-3" />
+            {/* Icons & Button */}
+            <div className="flex items-center justify-between">
+              {/* Icons */}
+              <div className="flex items-center">
+                <div className="bg-[#7C6A46] p-2 rounded-full text-white">
+                  <PiTelevisionSimple />
+                </div>
+                <div className="bg-[#7C6A46] p-2 rounded-full text-white mx-3">
+                  <PiShower />
+                </div>
+                <div className="bg-[#7C6A46] p-2 rounded-full text-white">
+                  <IoWifiOutline />
+                </div>
+              </div>
+              <button className="bg-[#7C6A46] text-white px-3 py-2 rounded hover:bg-[#8C6B27]">
+                Book Now
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}

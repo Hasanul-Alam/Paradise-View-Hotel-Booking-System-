0cc64f68aa586ac9f45458f74ec85fd33d12de43
Hasanul-Alam <superdev0000@gmail.com>
2024-11-16 16:02:38 +0600

    ready for deployment

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -7,13 +7,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBaEEDGkwkSgYkhBBkdxBvtEr2N2nq8FOU",
-  authDomain: "paradise-view.firebaseapp.com",
-  projectId: "paradise-view",
-  storageBucket: "paradise-view.firebasestorage.app",
-  messagingSenderId: "646897546477",
-  appId: "1:646897546477:web:2b5ebbe6df304614fe88e4",
-  measurementId: "G-NBXRC0ZLTG"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
